ac38f59f6e260424980bc2ad2ab0bd40f25a7d48
codewuren <codewuren@gmail.com>
2023-10-29 16:02:16 +0800

    Rename some arguments

--- src/cfhelper.rs
+++ src/cfhelper.rs
@@ -20,8 +20,8 @@ impl Helper {
     }
 
     // read a file and return the contents as a Vec<String>
-    pub fn read_file_vec(&self, _filename: &String) -> Vec<String> {
-        let mut file = match File::open(_filename) {
+    pub fn read_file_vec(&self, filename: &String) -> Vec<String> {
+        let mut file = match File::open(filename) {
             Ok(file) => file,
             Err(_) => panic!("No such file!"),
         };
@@ -42,14 +42,14 @@ impl Helper {
     // }
 
     // write the contents in _vec into a file
-    pub fn write_file(&self, _filename: &String, _vec: Vec<String>) {
+    pub fn write_file(&self, _filename: &String, vec: Vec<String>) {
         // open the file
         let mut file = std::fs::File::create(_filename)
             .expect("create failed!");
         
         // loop _vec and put each line into the target file
-        for i in 0.._vec.len() {
-            file.write_all(&_vec[i].as_bytes())
+        for i in 0..vec.len() {
+            file.write_all(&vec[i].as_bytes())
                 .expect("write failed!");
             file.write_all("\n".as_bytes())
                 .expect("write failed!");
@@ -58,13 +58,13 @@ impl Helper {
     }
 
     // run a shell command
-    pub fn run_command(&self, _command: String) -> String {
-        println!("run command: {}", _command);
+    pub fn run_command(&self, command: String) -> String {
+        println!("run command: {}", command);
 
         // get the output and put them into str_output
         let output = Command::new("sh")
             .arg("-c")
-            .arg(_command)
+            .arg(command)
             .output()
             .expect("Wrong!");
         let str_output: String = String::from_utf8(output.stdout)
@@ -73,7 +73,7 @@ impl Helper {
     }
 
     // generate the template code
-    pub fn gen_code(&self, _filename: &mut String) {
+    pub fn gen_code(&self, filename: &mut String) {
         // get user_home
         let mut template_path: String = self.run_command(String::from("echo $HOME"));
 
@@ -91,12 +91,12 @@ impl Helper {
 
         // read the contents of the template and write them into the target file
         let file_content: Vec<String> = self.read_file_vec(&template_path);
-        self.write_file(&format!("{filename}.cpp", filename = _filename), file_content);
-        self.write_file(&format!("{filename}.input", filename = _filename), vec![]);
+        self.write_file(&format!("{filename}.cpp", filename = filename), file_content);
+        self.write_file(&format!("{filename}.input", filename = filename), vec![]);
     }
 
     // run a test
-    pub fn test(&self, _filename: &String) {
+    pub fn test(&self, filename: &String) {
         // note: we only support the .cpp extension name
         println!("test started...");
 
@@ -110,23 +110,23 @@ impl Helper {
             && ./{filename} \
             < {filename}.input \
             > {filename}.output && cat {filename}.output", 
-            filename = _filename
+            filename = filename
         );
         
         let mut output: String = self.run_command(command);
         println!("the output is:\n{}", output);
 
-        command = format!("cat {filename}.input", filename = _filename);
+        command = format!("cat {filename}.input", filename = filename);
 
         output = self.run_command(command);
 
         println!("the input is:\n{}", output);
     }
 
-    // warn a error and quit if _exit_yon is true
-    pub fn error(&self, _info: String, _exit_yon: bool) {
-        println!("{}", String::from(_info));
-        if _exit_yon {
+    // warn a error and quit if exit_yon is true
+    pub fn error(&self, info: String, exit_yon: bool) {
+        println!("{}", String::from(info));
+        if exit_yon {
             process::exit(-1);
         }
     }
@@ -137,8 +137,8 @@ impl Helper {
     }
 
     // get user info through CodeForces's API
-    pub fn get_user_info(&self, _username: &String) {
-        let username_vec: Vec<String> = vec!(String::from(_username));
+    pub fn get_user_info(&self, username: &String) {
+        let username_vec: Vec<String> = vec!(String::from(username));
 
         // this is equivalent to the Codeforces `user.info` API method.
         let x: CFUserCommand = CFUserCommand::Info { 
@@ -170,9 +170,9 @@ impl Helper {
     }
 
     // search problemset
-    pub fn search_problems(&self, _problem: &String) {
+    pub fn search_problems(&self, problem: &String) {
         let x: CFProblemsetCommand = CFProblemsetCommand::Problems { 
-            tags: Some(vec![String::from(_problem)]),
+            tags: Some(vec![String::from(problem)]),
             problemset_name: None
         };
 
